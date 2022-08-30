071b09a20bd7be93e880d0f7bf678b5b082c797b
Ray Lee <wooo519@naver.com>
2022-08-30 17:41:48 +0900

    chore : test new

--- project1/practice_1/urls.py
+++ project1/practice_1/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.hello),
     path('jwt/',views.jwtfuc),
-    path('get_data',views.get_data)
+    path('get_data',views.get_data),
+    path('post_data',views.post_data),
+    path('jwtfuc',views.jwtfuc)
 ]
