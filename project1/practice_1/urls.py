a829201ecf5ccc90c6e78f3fc49eff1d29d5f58f
Ray Lee <wooo519@naver.com>
2022-09-01 00:21:09 +0900

    feat : finish login & register

--- project1/practice_1/urls.py
+++ project1/practice_1/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('get_data',views.get_data),
     path('post_data',views.post_data),
     path('jwtfuc',views.jwtfuc),
-    path('login',views.login)
+    path('login',views.login),
+    path('signup/',views.sign_up),
+    path('all/',views.get_all_mem)
 ]
