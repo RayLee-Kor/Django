f63ba65e603f6de64199fe4742ddc9a570f669e8
USER <wooo519@naver.com>
2022-12-27 02:31:03 +0900

    settings

--- project1/practice_1/models.py
+++ project1/practice_1/models.py
@@ -3,14 +3,13 @@ import uuid
 
 
 class user(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(unique=True, max_length=20, null=True, blank=True)
+    id = models.AutoField(primary_key=True)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    username = models.CharField(unique=True, max_length=20, null=True, blank=True)
     alias = models.CharField(unique=True, max_length=20)
-    # password = models.CharField(unique=True, max_length=20)
     password = models.BinaryField(max_length=60)
-    salt = models.BinaryField(max_length=29)
+    salt = models.BinaryField(max_length=30)
     email = models.CharField(max_length=50)
     active = models.BooleanField(default=True)
-    autosave = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
